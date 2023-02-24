8a63211b2bea8a84831b9c248cc34c9b3598c0ce
acharayaP03 <acharyaP03@gmail.com>
2023-02-25 10:24:27 +1100

    use prisma client to create user

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -3,6 +3,9 @@
 @post Request
  */
 
+import { createUser } from "~~/server/db/users";
+import { userTransformers } from "~~/server/transformers/user";
+
 export default defineEventHandler( async (event) => {
     const body = await readBody(event);
 
@@ -13,9 +16,25 @@ export default defineEventHandler( async (event) => {
             statusCode: 400,
             statusMessage: 'Invalid fields'
         }))
-
     }
+
+    if(password !== repeatPassword){
+            return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: 'Password do not match.'
+        }))
+    }
+
+    const userData = {
+        username,
+        email,
+        password,
+        name,
+        profileImage: 'https://picsum.photos/200/200'
+    }
+
+    const user = await createUser(userData)
     return {
-        body
+        body: userTransformers(user)
     }
 })
