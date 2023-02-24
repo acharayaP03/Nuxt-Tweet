fc6b3f6141599016b251cf5b2905ea7913a79944
acharayaP03 <acharyaP03@gmail.com>
2023-02-24 22:46:20 +1100

    more side bar content

---
+++ components/MoreSideBar/CardItems.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-3 hover:bg-gray-100 cursor-pointer dark:hover:bg-dim-300" :class="wrapperClasses">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+const { twitterBorderColor, defaultTransition } = useTailwindDefaults();
+const wrapperClasses = computed(() => `${defaultTransition} ${twitterBorderColor}`)
+</script>
+
+<style scoped></style>
