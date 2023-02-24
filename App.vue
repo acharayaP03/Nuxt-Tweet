0e0f610135e3a19616d5a04a64f6fd361a6aecc7
acharayaP03 <acharyaP03@gmail.com>
2023-02-24 14:20:09 +1100

    Initial project init

---
+++ App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div :class="{'dark': darkMode}">
+      <h1 class="text-red-500 dark:text-dim-600">Twitter clone</h1>
+    </div>
+  </template>
+  <script setup lang="ts">
+  const title = ref('');
+  const darkMode = ref(true)
+
+  </script>
