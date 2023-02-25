716da9e44b2e4095e58d33341eb93e16963a36b4
acharayaP03 <acharyaP03@gmail.com>
2023-02-26 09:47:59 +1100

    env varialbles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss']
+    modules: ['@nuxtjs/tailwindcss'],
+    runtimeConfig:{
+        accessToken: process.env.ACCESS_TOKEN_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN_SECRET
+    }
 })
