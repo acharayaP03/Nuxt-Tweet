691b8cfd9e92db817f53805c02c13cb63e8b872a
acharayaP03 <acharyaP03@gmail.com>
2023-02-26 09:49:02 +1100

    create and set refresh token

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,7 +1,12 @@
 /***
  @UserLogin 
 */
+import bcrypt from 'bcrypt';
 import { getUserByUsername } from '~~/server/db/users';
+import { generateTokens, sendRefreshToken } from '~~/server/utils/tokenUtils';
+import { userTransformers } from '~~/server/transformers/user';
+import { createRefreshToken } from '~~/server/db/refreshToken';
+
 export default defineEventHandler(async (event) =>{ 
     const body = await readBody(event);
     const { username, password } = body;
@@ -22,12 +27,31 @@ export default defineEventHandler(async (event) =>{
         }));
     }
     // compare password
+    const comparePassword = await bcrypt.compare(password, user.password)
+    if(!comparePassword){
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: 'Your username or password is not valid.'
+        }));
+    }
+    /**
+        @generateTokens 
+        1.) access token
+        2.) refresh token: will be saved in cookie
+     */
+
+    const { accessToken, refreshToken } = generateTokens(user)
+    await createRefreshToken({
+        token: refreshToken,
+        userId: user.id
+    })
+
+    // http cookie only: only server will use this
+    sendRefreshToken(event, refreshToken);
     
-    // generate token
-
-
     return{
-        user
+        user: userTransformers(user),
+        access_token: accessToken
     }
 
 })
