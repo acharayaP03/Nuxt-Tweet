2ecc18c40258c1b4c7b6c10c4b67303b6dff0e1b
acharayaP03 <acharyaP03@gmail.com>
2023-02-26 11:14:21 +1100

    make a request to api

---
+++ composables/useAuth.js
@@ -0,0 +1,22 @@
+export default () => {
+    const login = ({ username, password }) => {
+        return new Promise(async (resolve, reject) =>{
+            try {
+                const data  = await $fetch('/api/auth/login',{
+                    method: 'POST',
+                    body: {
+                        username,
+                        password
+                    }
+                })
+
+                console.log(data);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+    }
+    return {
+        login
+    }
+}
