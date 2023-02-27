9e730406858a50fc99661715f14241e88b0d17fc
acharayaP03 <acharyaP03@gmail.com>
2023-02-27 17:46:42 +1100

    cloudinary uploader

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+
+export const uploadToCloudinary = () => {
+    return new Promise((resolve, reject) =>{
+        const config = useRuntimeConfig();
+        // cloudinary credentials
+        cloudinary.config({
+            cloud_name: config.CLOUDINARY_CLOUD_NAME,
+            api_key: config.CLOUDINARY_API_KEY,
+            api_secret: config.CLOUDINARY_API_SECRET
+        });
+
+        //process media files 
+        cloudinary.uploader.upload(image, (error, data) => {
+            if(error) reject(error);
+
+            resolve(data)
+        })
+    })
+}
