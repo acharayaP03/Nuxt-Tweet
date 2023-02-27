27d56aa9397dd8dae697d54a2b4b096e1407d7f8
acharayaP03 <acharyaP03@gmail.com>
2023-02-28 08:34:53 +1100

    add loading spinner

--- components/Tweet/Form.vue
+++ components/Tweet/Form.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
+    <div v-if='loading' class='flex items-center justify-center py-6'>
+        <UISpinner />
+    </div>
+    <div v-else>
        <TweetInput @onSubmit="handleFormSubmit"/>
     </div>
 </template>
@@ -8,8 +11,10 @@
 import useTweets from '../../composables/useTweets'
 
 const { postTweet } = useTweets();
+const loading = ref(false)
 // data comes from child component emitter ..
 const handleFormSubmit = async ({ text, mediaFiles }) => {
+    loading.value = true;
     try {
         const responseTweet = await postTweet({
             text,
@@ -20,6 +25,8 @@ const handleFormSubmit = async ({ text, mediaFiles }) => {
 
     } catch (error) {
         console.log(error);
+    } finally {
+        loading.value = flase
     }
 }
 </script>
