632ee238a1117d276e53116174f6ea23d987f419
acharayaP03 <acharyaP03@gmail.com>
2023-02-27 18:16:10 +1100

    import api key for cloudinary

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     runtimeConfig:{
         accessToken: process.env.ACCESS_TOKEN_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN_SECRET
+        refreshToken: process.env.REFRESH_TOKEN_SECRET,
+        cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+        cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
     }
 })
