df8192ff6b50354463393e1f78a402854bce3b1a
acharayaP03 <acharyaP03@gmail.com>
2023-02-27 22:27:01 +1100

    post tweet

--- components/Tweet/Form.vue
+++ components/Tweet/Form.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script setup>
+const { postTweet } = useTweets();
 // data comes from child component emitter ..
-const handleFormSubmit = (data) => {
-    alert(JSON.stringify(data))
+const handleFormSubmit = async (data) => {
+    try {
+        const responseTweet = await postTweet(data);
+
+        console.log(responseTweet);
+    } catch (error) {
+        console.log(error);
+    }
 }
 </script>
