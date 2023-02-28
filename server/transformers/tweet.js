a0ffe108e854a8b0052a0ebe3c743833e0388a50
acharayaP03 <acharyaP03@gmail.com>
2023-02-28 11:20:46 +1100

    Tweet transformer

--- server/transformers/tweet.js
+++ server/transformers/tweet.js
@@ -1,6 +1,18 @@
-export const tweetTransformers = (tweet) => {
+import { mediaFilesTransformer } from '~/server/transformers/mediaFiles'
+import { userTransformers } from '~/server/transformers/user';
+import human from 'human-time';
+
+
+export const tweetTransformers = (tweet)=> {
+
     return{
-        id: tweet._id,
-        text: tweet.text
+        id: tweet.id,
+        text: tweet.text,
+        author: !!tweet.author ? userTransformers(tweet.author) : null,
+        medaiFiles: !!tweet.medaiFiles ? tweet.medaiFiles.map(mediaFilesTransformer) : [],
+        replies: !!tweet.replies ? tweet.replies.map(tweetTransformers): [],
+        replTo: !!tweet.replTo ? tweetTransformers(tweet.replTo) : null,
+        repliesCount: !!tweet.replies ? tweet.replies.length : 0,
+        postedAtHuman: human(tweet.createdAt)
     }
 }
