86998b32e3ab7d0e46e87daf025a1e2d8cdab146
acharayaP03 <acharyaP03@gmail.com>
2023-02-28 20:58:50 +1100

    get tweet by id

--- pages/status/[id].vue
+++ pages/status/[id].vue
@@ -9,5 +9,26 @@
 </template>
 
 <script setup>
-const loading = ref(false)
+
+const loading = ref(false);
+
+//get id from route params
+function getTweetIdFromRoute() {
+    return useRoute().params.id;
+}
+
+// async function getTweets () {
+//     loading.value = true;
+//     try{
+//         const response = await getTweetById(getTweetIdFromRoute());
+//
+//         console.log(response)
+//     }catch(error){
+//         console.log(error);
+//     } finally {
+//         loading.value = false
+//     }
+// }
+
+
 </script>
