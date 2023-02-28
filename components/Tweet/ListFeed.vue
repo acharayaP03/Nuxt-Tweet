ec28024758ccdcfd612934e3e563cdb2c057afb9
acharayaP03 <acharyaP03@gmail.com>
2023-02-28 12:10:28 +1100

    Fetch all tweets

---
+++ components/Tweet/ListFeed.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1> List feed</h1>
+
+    <ul>
+        <li v-for='tweet in tweets'>{{ tweet.text }}</li>
+    </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+    tweets: {
+        type: Array,
+        required: true
+    }
+})
+</script>
