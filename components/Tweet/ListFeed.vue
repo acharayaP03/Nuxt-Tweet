6bcf51b10e0967958175c020543a68eaa7952e4c
acharayaP03 <acharyaP03@gmail.com>
2023-02-28 15:10:25 +1100

    tweet list item

--- components/Tweet/ListFeed.vue
+++ components/Tweet/ListFeed.vue
@@ -1,16 +1,30 @@
 <template>
-    <h1> List feed</h1>
-
-    <ul>
-        <li v-for='tweet in tweets'>{{ tweet.text }}</li>
-    </ul>
+    <div v-if='tweetExist' class='p-4'>
+        <p class='text-center text-gray-500 capitalize'>
+            sorry there are no tweets, please create one
+        </p>
+    </div>
+    <div
+        v-else
+        class='pb-4 border-b hover:bg-gray-100 cursor-pointer dark:hover:bg-dim-300'
+        :class='[twitterBorderColor, defaultTransition]'
+        v-for='tweet in props.tweets'
+        :key='tweet.id'
+    >
+        <TweetItems :tweet='tweet'/>
+    </div>
 </template>
 
 <script setup>
+import useTailwindDefaults from '../../composables/useTailwindDefaults'
+
+const { twitterBorderColor, defaultTransition } = useTailwindDefaults()
 const props = defineProps({
     tweets: {
         type: Array,
         required: true
     }
 })
+
+const tweetExist = computed(() => props.tweets && props.tweets.length === 0)
 </script>
